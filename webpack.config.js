52a6aa686084bdb46a68fbd276d80eb2186b19c6
Danil <Danil.nktel@gmail.com>
2017-04-18 14:13:00 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+	entry: './src/App.jsx',
+	output: {
+		path: __dirname + "/public",
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+		{
+			test: /\.jsx?$/,
+			exclude: [/node_modules/],
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets:['react', 'es2015']
+				}
+			}]
+		},
+		{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				fallback: 'style-loader',
+				use: 'css-loader'
+			})
+		}
+		]
+	},
+	plugins: [
+	  new ExtractTextPlugin('styles.css')
+	]
+}
